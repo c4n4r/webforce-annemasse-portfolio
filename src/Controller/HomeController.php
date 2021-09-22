c71a73df1dac3cefd6519c92008932c9671abd64
c4n4r <Carotte1988->
2021-09-22 10:18:09 +0200

    création des entities et des fixtures

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,13 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Skill;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
 
     public function indexAction() {
-        return $this->render('home.html.twig');
+
+        //aller récupérer 3 skills au pif
+        $skills = $this->getDoctrine()->getRepository(Skill::class)->findAll();
+        //je randomise ma liste de compétences
+        shuffle($skills);
+        //je cut le tableau pour garder les 3 premier
+        $skills = array_slice($skills,0,3);
+        return $this->render('home.html.twig',
+            ['skills' => $skills]);
     }
 
 }
